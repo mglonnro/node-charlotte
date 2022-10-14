b0a12931ea5f5db0f5bcc00cc3e58b32abb05150
Mikael Lönnroth <mikael@humeko.com>
2022-10-14 23:44:40 +0300

    Added export of Average.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import CharlotteAPI from "./src/oldapi.js";
 import { Data } from "./src/data.js";
+import { Average } from "./src/average.js";
 
 export default CharlotteAPI;
 
-export { CharlotteAPI, Data };
+export { CharlotteAPI, Data, Average };
